8a00d7f4704e9d257ec0db1cb64807016811944e
Vivek Shindhe <vivek.shindhe@razorpay.com>
2025-02-28 09:20:59 +0530

    version update 1.4.0

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/vivek.shindhe/fvm/versions/3.22.0"
+export "FLUTTER_ROOT=/Users/vivek.shindhe/development/flutter"
 export "FLUTTER_APPLICATION_PATH=/Users/vivek.shindhe/Razorpay/Flutter/razorpay-flutter/example"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
